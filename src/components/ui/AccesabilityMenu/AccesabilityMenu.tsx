56de692c91de9f11aefdecd8fe3775d8f07b2ac0
Viacheslav Borysov <s.borisovbar@gmail.com>
2024-10-13 23:12:48 +0300

    test accesability

---
+++ src/components/ui/AccesabilityMenu/AccesabilityMenu.tsx
@@ -0,0 +1,77 @@
+'use client';
+import React from 'react';
+import { useAccessibility } from '@/context/AccesabilityContext';
+
+type AccessibilitySettings = {
+  textSize: 'text-medium' | 'text-large';
+  highContrast: boolean;
+  highlightLinks: boolean;
+  largeCursor: boolean;
+};
+
+const AccessibilityMenu = () => {
+  const { settings, setSettings } = useAccessibility() as {
+    settings: AccessibilitySettings;
+    setSettings: React.Dispatch<React.SetStateAction<AccessibilitySettings>>;
+  };
+
+  const handleTextSizeChange = () => {
+    setSettings(prevSize => ({
+      ...prevSize,
+      textSize:
+        prevSize.textSize === 'text-medium' ? 'text-large' : 'text-medium',
+    }));
+  };
+
+  const toggleHighContrast = () => {
+    setSettings(prev => ({ ...prev, highContrast: !prev.highContrast }));
+  };
+
+  const toggleHighlightLinks = () => {
+    setSettings(prev => ({ ...prev, highlightLinks: !prev.highlightLinks }));
+  };
+
+  const toggleLargeCursor = () => {
+    setSettings(prev => ({ ...prev, largeCursor: !prev.largeCursor }));
+  };
+
+  return (
+    <div className="fixed right-0 top-0 bg-gray-100 p-4 shadow-lg">
+      <h2 className="mb-4 text-lg font-bold">Налаштування доступності</h2>
+
+      <button
+        onClick={handleTextSizeChange}
+        className="mb-2 block rounded bg-blue-500 p-2 text-white"
+      >
+        {settings.textSize === 'text-medium'
+          ? 'Збільшити текст'
+          : 'Зменшити текст'}
+      </button>
+
+      <button
+        onClick={toggleHighContrast}
+        className={`mb-2 block rounded p-2 ${settings.highContrast ? 'bg-yellow-500' : 'bg-gray-500'} text-white`}
+      >
+        {settings.highContrast ? 'Вимкнути контраст' : 'Увімкнути контраст'}
+      </button>
+
+      <button
+        onClick={toggleHighlightLinks}
+        className={`mb-2 block rounded p-2 ${settings.highlightLinks ? 'bg-yellow-500' : 'bg-gray-500'} text-white`}
+      >
+        {settings.highlightLinks
+          ? 'Вимкнути підсвічування'
+          : 'Підсвітити посилання'}
+      </button>
+
+      <button
+        onClick={toggleLargeCursor}
+        className={`mb-2 block rounded p-2 ${settings.largeCursor ? 'bg-yellow-500' : 'bg-gray-500'} text-white`}
+      >
+        {settings.largeCursor ? 'Зменшити курсор' : 'Збільшити курсор'}
+      </button>
+    </div>
+  );
+};
+
+export default AccessibilityMenu;
---
+++ src/context/AccesabilityContext.js
@@ -0,0 +1,72 @@
+'use client';
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const AccessibilityContext = createContext(null);
+
+export const useAccessibility = () => {
+  const context = useContext(AccessibilityContext);
+  if (!context) {
+    throw new Error(
+      'useAccessibility must be used within an AccessibilityProvider',
+    );
+  }
+  return context;
+};
+
+export const AccessibilityProvider = ({ children }) => {
+  const [settings, setSettings] = useState({
+    textSize: 'text-medium',
+    highContrast: false,
+    highlightLinks: false,
+    largeCursor: false,
+  });
+
+  useEffect(() => {
+    const savedSettings = JSON.parse(
+      localStorage.getItem('accessibilitySettings') || '{}',
+    );
+    if (savedSettings) {
+      setSettings(savedSettings);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('accessibilitySettings', JSON.stringify(settings));
+    applySettings(settings);
+  }, [settings]);
+
+  const applySettings = newSettings => {
+    const paragraphs = document.querySelectorAll('p');
+    const links = document.querySelectorAll('a');
+
+    paragraphs.forEach(p => {
+      p.className = `${newSettings.textSize} transition-font-size`; // Додайте клас анімації до всіх <p>
+    });
+
+    links.forEach(link => {
+      link.className = `${newSettings.textSize} transition-font-size`;
+      link.style.backgroundColor = newSettings.highlightLinks ? 'yellow' : '';
+      link.style.textDecoration = newSettings.highlightLinks
+        ? 'underline'
+        : 'none';
+    });
+
+    document.body.classList.toggle('high-contrast', newSettings.highContrast);
+    document.documentElement.style.cursor = newSettings.largeCursor
+      ? 'url("../../public/images/pointer.png")'
+      : 'auto';
+
+    document.querySelectorAll('a').forEach(link => {
+      link.style.backgroundColor = newSettings.highlightLinks ? 'yellow' : '';
+      link.style.textDecoration = newSettings.highlightLinks
+        ? 'underline'
+        : 'none';
+    });
+  };
+
+  return (
+    <AccessibilityContext.Provider value={{ settings, setSettings }}>
+      {children}
+    </AccessibilityContext.Provider>
+  );
+};
