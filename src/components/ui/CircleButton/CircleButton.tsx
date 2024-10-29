f26fed22eab6fb54cd3ee33dc82ef75dee325624
Viacheslav Borysov <s.borisovbar@gmail.com>
2024-10-30 00:42:36 +0200

    fix component Circle button

--- src/components/ui/CircleButton/CircleButton.tsx
+++ src/components/ui/CircleButton/CircleButton.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import React from 'react';
-
 import { CircleButtonProps } from './types';
 
-export const CircleButton: React.FC<CircleButtonProps> = ({ children }) => {
+export const CircleButton: React.FC<CircleButtonProps> = ({
+  children,
+  action,
+}) => {
   return (
     <button
-      className="sm:flex sm:h-[44px] sm:w-[44px] sm:items-center sm:justify-center sm:rounded-full sm:bg-bgSlate sm:p-[8px_14px]"
+      className="flex size-11 items-center justify-center rounded-full bg-bgSlate p-2"
       type="button"
+      onClick={action}
     >
       {children}
     </button>
