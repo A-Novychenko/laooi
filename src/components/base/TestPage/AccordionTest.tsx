760336a7921d672d127fca2793e998afb13c93ee
A-Novychenko <A.Novychenko@outlook.com>
2024-10-30 02:00:30 +0200

    fix

---
+++ src/components/base/TestPage/AccordionTest.tsx
@@ -0,0 +1,32 @@
+import { Accordeon } from '@/components/ui/Accordeon/Accordeon';
+
+const testData = [
+  {
+    title: 'Що таке Луганська Асоціація організацій осіб з інвалідністю?',
+    text: 'ЛАООІ є добровільним громадським об’єднанням, членами якого є фізичні особи та юридичні особи приватного права, створеним для здійснення та захисту прав i свобод людини та громадянина, задоволення суспільних, зокрема, економічних, соціальних, культурних та інших інтересів своїх членів та/або інших осіб',
+  },
+  {
+    title: 'уганська Асоціація організацій осіб',
+    text: 'sdaasdasdasdasdsadasdasdasddasd',
+  },
+  {
+    title: 'уганська Асоціація організацій осіб',
+    text: 'sdaasdasdasdasdsadasdasdasddasd',
+  },
+  {
+    title: 'уганська Асоціація організацій осіб',
+    text: 'sdaasdasdasdasdsadasdasdasddasd',
+  },
+  {
+    title: 'уганська Асоціація організацій осіб',
+    text: 'sdaasdasdasdasdsadasdasdasddasd',
+  },
+];
+
+export const AccordionTest = () => {
+  return (
+    <div className="flex justify-center">
+      <Accordeon data={testData} />
+    </div>
+  );
+};
