08ae535af218846044a9eb9fca428fde373c67fc
Viacheslav Borysov <s.borisovbar@gmail.com>
2024-11-03 16:32:47 +0200

    fix button

--- src/components/ui/ScrollToTopBtn/ScrollToTopBtn.tsx
+++ src/components/ui/ScrollToTopBtn/ScrollToTopBtn.tsx
@@ -1,10 +1,27 @@
 'use client';
+import { useEffect, useState } from 'react';
 
 import { CircleButton } from '../CircleButton';
 
 import Arrow from '~/icons/arrowBtnIcon.svg';
 
 export const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 100) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -13,7 +30,10 @@ export const ScrollToTopButton = () => {
   };
 
   return (
-    <CircleButton action={scrollToTop} className="ml-auto">
+    <CircleButton
+      action={scrollToTop}
+      className={`scroll-to-top group ${isVisible ? 'visible' : ''} ml-auto mt-3`}
+    >
       <Arrow className="-rotate-45" width={24} height={24} />
     </CircleButton>
   );
