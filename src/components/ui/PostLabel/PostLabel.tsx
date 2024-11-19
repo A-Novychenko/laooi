096223897d4ecee9ae5912745e55c82f5286a1dc
Viacheslav Borysov <s.borisovbar@gmail.com>
2024-11-20 01:33:24 +0200

    fixed PostLabel

--- src/components/ui/PostLabel/PostLabel.tsx
+++ src/components/ui/PostLabel/PostLabel.tsx
@@ -1,18 +1,20 @@
-import { getLabelColor } from '@/utils/getLabelColor';
 import { cn } from '@/utils/cn';
 
 import { PostLabelProps } from './types';
 
-import styles from './PostLabel.module.css';
-
 export const PostLabel: React.FC<PostLabelProps> = ({ label, children }) => {
-  const textColor = getLabelColor(label);
+  const textColor = cn('text-base/normal font-normal, capitalize', {
+    'text-textBlue':
+      label.toLowerCase() === 'news' || label.toLowerCase() === 'новини',
+    'text-textGreen':
+      label.toLowerCase() === 'events' || label.toLowerCase() === 'події',
+    'text-textRed':
+      label.toLowerCase() === 'articles' || label.toLowerCase() === 'статті',
+  });
 
   return (
     <div className="absolute right-2 top-2 flex w-[107px] items-center justify-center rounded-3xl bg-bgLight px-6 py-3">
-      <p className={cn('text-base/normal font-normal', styles[textColor])}>
-        {children}
-      </p>
+      <p className={textColor}>{children}</p>
     </div>
   );
 };
