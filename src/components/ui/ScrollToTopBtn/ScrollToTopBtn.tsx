47b5667a3e1405df41b227b127b055098c95c052
Viacheslav Borysov <s.borisovbar@gmail.com>
2024-11-19 23:36:20 +0200

    fix fixing

--- src/components/ui/ScrollToTopBtn/ScrollToTopBtn.tsx
+++ src/components/ui/ScrollToTopBtn/ScrollToTopBtn.tsx
@@ -32,7 +32,7 @@ export const ScrollToTopButton = () => {
   return (
     <CircleButton
       action={scrollToTop}
-      className={`${isVisible ? 'flex' : 'visually-hidden'} absolute bottom-[10px] right-4 ml-auto md:bottom-[26px] md:right-10 xl:bottom-[42px]`}
+      className={`${isVisible ? 'flex' : 'visually-hidden'} absolute bottom-[-54px] right-4 ml-auto md:bottom-[-70px] md:right-10 xl:bottom-[-86px]`}
     >
       <Arrow className="-rotate-45" width={24} height={24} />
     </CircleButton>
--- src/sections/FAQSection/FAQSection.tsx
+++ src/sections/FAQSection/FAQSection.tsx
@@ -7,8 +7,8 @@ export const FAQSection: React.FC<FAQSectionProps> = ({ dict }) => {
   const { title, items } = dict.FAQSection;
 
   return (
-    <section className="container relative pb-16 pt-8 md:pb-24 md:pt-12 xl:pb-32 xl:pt-16">
-      <div className="container">
+    <section className="pb-16 pt-8 md:pb-24 md:pt-12 xl:pb-32 xl:pt-16">
+      <div className="container relative">
         <div className="xl:flex xl:justify-center">
           <div className="xl:inline-block">
             <Title className="mb-4 xl:w-full">{title}</Title>
