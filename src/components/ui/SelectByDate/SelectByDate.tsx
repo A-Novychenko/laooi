7c5a55b354236a2f49d58f0280c0e875dd19c310
Aleksandr Novychenko <A.Novychenko@outlook.com>
2024-12-11 03:41:30 +0200

    Update SelectByDate.tsx

--- src/components/ui/SelectByDate/SelectByDate.tsx
+++ src/components/ui/SelectByDate/SelectByDate.tsx
@@ -6,10 +6,12 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import ArrowIcon from '~/icons/arrowDown.svg';
 
 import { SelectByDateProps, SortDate } from './types';
+
 export const SelectByDate: React.FC<SelectByDateProps> = ({
   selectSortByDate,
 }) => {
   const [selectedSort, setSelectedSort] = useState<SortDate | ''>('');
+  
   const router = useRouter();
   const searchParams = useSearchParams();
 
@@ -60,6 +62,7 @@ export const SelectByDate: React.FC<SelectByDateProps> = ({
           className="flex w-full cursor-pointer justify-between rounded-full bg-bgLightSlate px-6 py-4 text-base/normal font-semibold focus-within:bg-bgSlate hover:bg-bgSlate xl:text-lg"
         >
           <p id="sort-title">{getSortTitle(selectedSort)}</p>
+          
           <ArrowIcon
             width={24}
             height={24}
