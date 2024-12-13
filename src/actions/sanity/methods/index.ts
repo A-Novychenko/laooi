c70cb24d0d76312d8d2de7d0da3ff974a3df6071
Viacheslav Borysov <s.borisovbar@gmail.com>
2024-12-13 23:49:13 +0200

    del not usable file

--- src/actions/sanity/methods/index.ts
+++ src/actions/sanity/methods/index.ts
@@ -2,8 +2,6 @@ export * from './getAllPosts';
 export * from './getPostBySlug';
 export * from './getAllMediaItems';
 export * from './getLatestMediaItems';
-export * from './getTotalCountPosts';
-export * from './getSearchPostCount';
 export * from './getDocuments';
 export * from './getDocuments';
 export * from './getLatestPosts';
--- src/actions/sanity/queries/getSearchPostCountQuery.ts
+++
@@ -1,8 +0,0 @@
-export const getSearchPostCountQuery = (
-  search: string,
-  lang: 'uk' | 'en',
-  postType?: 'news' | 'articles' | 'events',
-) => {
-  const typeFilter = postType ? `&& postType == "${postType}"` : '';
-  return `count(*[_type == "Post" && (title.${lang} match "${search}*" || body.${lang} match "${search}*") ${typeFilter}])`;
-};
