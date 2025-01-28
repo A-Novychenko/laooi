087b748ff2f1ada60bc215d5d234f5e35bf0d606
Viacheslav Borysov <s.borisovbar@gmail.com>
2025-01-29 01:17:31 +0200

    fix semantic in main areas section

--- src/sections/MainAreasSection/MainAreasSection.tsx
+++ src/sections/MainAreasSection/MainAreasSection.tsx
@@ -1,42 +1,51 @@
 import { Title } from '@/components/ui';
+
+import { cn } from '@/utils/cn';
+
+import styles from './MainAreasSection.module.css';
+
 import { MainAreasSectionProps } from './types';
-import Image from 'next/image';
 
 export const MainAreasSection: React.FC<MainAreasSectionProps> = ({ dict }) => {
   const {
     title,
-    logo: { src, alt },
+    logo: { alt },
     areas,
   } = dict.mainAreasSection;
+
+  const newAreas = [{ title: alt, description: alt }, ...areas];
+
   return (
     <section className="section">
       <div className="container">
         <Title className="mb-4">{title}</Title>
+
         <ul className="flex flex-col gap-[16px] md:flex-row md:flex-wrap md:justify-center">
-          <li className="md:w-[308px] xl:w-[416px]">
-            <div className="h-[279px] overflow-hidden rounded-3xl md:h-[307px] xl:h-[296]">
-              <Image
-                src={src}
-                alt={alt}
-                width={416}
-                height={296}
-                className="size-full object-cover"
-              />
-            </div>
-          </li>
-          {areas.map((item, idx) => (
+          {newAreas.map((item, idx) => (
             <li
               key={idx}
-              className="rounded-3xl bg-bgLightSlate p-[24px] md:min-h-[307px] md:w-[308px] md:p-[32px] xl:min-h-[296px] xl:w-[416px] xl:p-[40px]"
+              className={cn(
+                'min-h-[279px] rounded-3xl bg-bgLightSlate p-[24px] md:min-h-[307px] md:w-[308px] md:p-[32px] xl:min-h-[296px] xl:w-[416px] xl:p-[40px]',
+                styles.cardAreas,
+              )}
             >
               <Title
                 tag={'h3'}
                 style={'third'}
-                className="mb-2 uppercase text-textMenuAccent md:text-[20px] xl:text-[24px]"
+                className={cn(
+                  'mb-2 uppercase text-textMenuAccent md:text-[20px] xl:text-[24px]',
+                  idx === 0 && 'visually-hidden',
+                )}
               >
                 {item.title}
               </Title>
-              <p className="text-base/[1.38] md:text-lg/[1.39]">
+
+              <p
+                className={cn(
+                  'text-base/[1.38] md:text-lg/[1.39]',
+                  idx === 0 && 'visually-hidden',
+                )}
+              >
                 {item.description}
               </p>
             </li>
