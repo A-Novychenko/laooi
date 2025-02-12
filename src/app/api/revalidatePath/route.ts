23253b342e0c06fe37f8eefbcdc14516076ee66b
A-Novychenko <A.Novychenko@outlook.com>
2025-02-12 12:50:34 +0200

    first test cache-settings

---
+++ src/app/api/revalidatePath/route.ts
@@ -0,0 +1,17 @@
+import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const secret = req.nextUrl.searchParams.get('secret');
+  if (secret !== process.env.SANITY_REVALIDATE_SECRET) {
+    return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
+  }
+
+  const path = req.nextUrl.searchParams.get('path');
+  if (!path) {
+    return NextResponse.json({ message: 'Path is required' }, { status: 400 });
+  }
+
+  revalidatePath(path);
+  return NextResponse.json({ revalidated: true, path });
+}
